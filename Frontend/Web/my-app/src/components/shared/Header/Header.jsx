f79d7c9a24ad7256433f77977441927e58456717
OneClock1 <toly1539@gmail.com>
2023-02-21 16:37:51 +0200

    Chandes Header.jsx

--- Frontend/Web/my-app/src/components/shared/Header/Header.jsx
+++ Frontend/Web/my-app/src/components/shared/Header/Header.jsx
@@ -1,9 +1,11 @@
 import "./Header.css"
 
 const Header = () => {
-    return{
-         
-    }
-}
+    return <div>
+        <h1>HEllo</h1>
+        <h1>Maltsev Anatoly</h1>
+    </div>
+   
+};
 
 export default Header;
